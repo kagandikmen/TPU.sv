bbdcd535656d7453782b5dead450b442976b99ef
Kagan Dikmen <kagandikmen@outlook.com>
2024-10-05 05:33:21 +0200

    add accumulate load counter

---
+++ src/tpu/rtl/acc_load_ctr.sv
@@ -0,0 +1,79 @@
+// Accumulate load counter
+// Created: 2024-10-05
+// Modified: 2024-10-05
+
+// Copyright (c) 2024 Kagan Dikmen
+// See LICENSE for details
+
+`include "../lib/tpu_pkg.sv"
+
+import tpu_pkg::*;
+
+module acc_load_ctr
+    #(
+        parameter int COUNTER_WIDTH = 32,
+        parameter int MATRIX_WIDTH = 14
+    )(
+        input   logic clk,
+        input   logic rst,
+        input   logic enable,
+
+        input   logic [COUNTER_WIDTH-1:0] start_val,
+        input   logic load,
+
+        output  logic [COUNTER_WIDTH-1:0] ctr_val
+    );
+
+    logic [COUNTER_WIDTH-1:0] ctr_input_cs = '{default: 0};
+    logic [COUNTER_WIDTH-1:0] ctr_input_ns;
+
+    logic [COUNTER_WIDTH-1:0] start_val_cs = '{default: 0};
+    logic [COUNTER_WIDTH-1:0] start_val_ns;
+
+    logic [COUNTER_WIDTH-1:0] input_pipe_cs = '{default: 0};
+    logic [COUNTER_WIDTH-1:0] input_pipe_ns;
+
+    logic [COUNTER_WIDTH-1:0] ctr_cs = '{default: 0};
+    (* use_dsp = "yes" *) logic [COUNTER_WIDTH-1:0] ctr_ns;
+
+    logic load_cs = 0;
+    logic load_ns;
+
+    
+    // pipeline
+    assign load_ns = load;
+
+    assign start_val_ns = start_val;
+    
+    assign input_pipe_ns = load ? start_val : '{0: 1, default: 0};
+    assign ctr_input_ns = input_pipe_cs;
+
+    assign ctr_ns = (ctr_cs == $unsigned(MATRIX_WIDTH-1) + $unsigned(start_val_cs)) ? start_val_cs : ($unsigned(ctr_cs) + $unsigned(ctr_input_cs));
+    assign ctr_val = ctr_cs;
+
+
+    // next state logic
+    always_ff @(posedge clk)
+    begin
+        if(rst)
+        begin
+            ctr_input_cs <= '{default: 0};
+            input_pipe_cs <= '{default: 0};
+            load_cs <= 0;
+        end
+        else if(enable)
+        begin
+            ctr_input_cs <= ctr_input_ns;
+            input_pipe_cs <= input_pipe_ns;
+            load_cs <= load_ns;
+        end
+
+        if(load_cs)
+            ctr_cs <= '{default: 0};
+        else if(enable)
+            ctr_cs <= ctr_ns;
+
+        if(load)
+            start_val_cs <= start_val_ns;
+    end
+endmodule
---
+++ src/tpu/sim/tb_acc_load_ctr.sv
@@ -0,0 +1,113 @@
+// Testbench for accumulate load counter
+// Created: 2024-10-05
+// Modified: 2024-10-05
+
+// Copyright (c) 2024 Kagan Dikmen
+// See LICENSE for details
+
+`include "../lib/tpu_pkg.sv"
+`include "../rtl/acc_load_ctr.sv"
+
+import tpu_pkg::*;
+
+module tb_acc_load_ctr
+    #(
+    )(
+    );
+
+    localparam COUNTER_WIDTH = 8;
+    localparam MATRIX_WIDTH = 4;
+
+    logic clk, rst, enable, load;
+    logic [COUNTER_WIDTH-1:0] start_val, ctr_val;
+
+    acc_load_ctr #(
+        .COUNTER_WIDTH(COUNTER_WIDTH),
+        .MATRIX_WIDTH(MATRIX_WIDTH)
+    ) dut (
+        .clk(clk),
+        .rst(rst),
+        .enable(enable),
+        .start_val(start_val),
+        .load(load),
+        .ctr_val(ctr_val)
+    );
+
+    // clock generation
+    initial
+    begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    // stimuli
+    initial
+    begin
+        // initialize signals
+        rst <= 0;
+        enable <= 0;
+        start_val <= 0;
+        load <= 0;
+
+        // toggle reset
+        repeat (2) @(posedge clk);
+        rst <= 1;
+        @(posedge clk);
+        rst <= 0;
+
+        // test 1: try incrementing without enabling
+        @(posedge clk);
+        load <= 1;
+        @(posedge clk);
+        load <= 0;
+        repeat (3) @(posedge clk);
+        if(ctr_val != 0)
+            $fatal("Test 1.1 failed");
+        @(posedge clk);
+        load <= 1;
+        start_val <= 5;
+        @(posedge clk);
+        load <= 0;
+        repeat (3) @(posedge clk);
+        if(ctr_val != 0)
+            $fatal("Test 1.2 failed");
+
+        // test 2: enable with load
+        @(posedge clk);
+        enable <= 1;
+        repeat (2) @(posedge clk);
+        for (int i=0; i<5; i++)
+        begin
+            #1;
+            if(ctr_val != i)
+                $fatal("Test 2 failed");
+            @(posedge clk);
+        end
+
+        // test 3: load start_val
+        @(posedge clk);
+        load <= 1;
+        start_val <= 11;
+        @(posedge clk);
+        load <= 0;
+        repeat (3) @(posedge clk);
+        if(ctr_val != 11)
+            $fatal("Test 3 failed");
+
+        // test 4: accumulation
+        for(int i=0; i<30; i++)
+        begin
+            if(ctr_val != start_val + (i%MATRIX_WIDTH))
+                $fatal("Test 4 failed");
+            @(posedge clk); 
+        end
+
+        repeat (16) @(posedge clk);
+        @(posedge clk);
+        enable <= 0;
+
+        #50;
+        $display("Tests completed successfully");
+        $finish;
+    end
+endmodule
